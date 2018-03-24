cc56e26f40897409a2e2ab012dfc460fff0727f0
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-24 13:41:12 +0530

    Create 1. Print, Puts, Variables, Arithmetic, if else.rb

---
+++ src/1. Print, Puts, Variables, Arithmetic, if else.rb
@@ -0,0 +1,33 @@
+Print "str"   : print string without \n 
+Puts "str"    : print string with newline 
+ 
+in = gets.chomp 
+puts "name: #[in]" 
+ 
+X = 23 
+Y = "str" 
+ 
++, -, *, /, %, ** 
+ 
+If x>9 
+puts "ok" 
+elsif x<10 
+Puts "no" 
+else 
+Puts "fdhf" 
+ 
+ 
+While loop 
+while x<10 
+x+=1 
+
+
+ 
+LOOP 
+i=0 
+loop do 
+    i+=1 
+    next if i==3 
+    puts i 
+    break if i==10 
+end 
